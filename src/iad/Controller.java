68f0420616ed8c9513fd3a51fcbe071d2b9775a5
Dominik Cegiełka <224478@edu.p.lodz.pl>
2020-03-13 00:38:54 +0100

    added import handler from file to map

--- src/iad/Controller.java
+++ src/iad/Controller.java
@@ -4,8 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
+    private Data data;
+
     @FXML
     private ChoiceBox<String> choiceSeparator;
     ObservableList<String> separatorList = FXCollections.observableArrayList(",",".",";");
@@ -15,5 +21,14 @@ public class Controller {
         choiceSeparator.setItems(separatorList);
         choiceSeparator.setValue(",");
     }
+
+    @FXML
+    public void openFile(){
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Import data");
+        File file = fileChooser.showOpenDialog(new Stage());
+        data = new Data(file,choiceSeparator.getValue());
+    }
+
 
 }
--- src/iad/Data.java
+++ src/iad/Data.java
@@ -1,5 +1,84 @@
 package iad;
 
-public class Data {
+import javax.xml.crypto.dom.DOMCryptoContext;
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class Data {
+    private File file;
+    private String separator;
+    public HashMap<ArrayList<ArrayList<Double>>, String> data;
+    public SortedSet<String> classTypes;
+    public int numberOfAttribute;
+
+    public Data(File file, String separator) {
+        this.file = file;
+        this.separator = separator;
+        getClassType();
+        getDataOfFile();
+    }
+
+    private void getClassType() {
+        String currentLine;
+        String[] separatedDataLine;
+        classTypes = new TreeSet<String>();
+
+        try {
+            BufferedReader buffor = new BufferedReader(new FileReader(file));
+            currentLine = buffor.readLine();
+            separatedDataLine = currentLine.split(separator);
+            classTypes.add(separatedDataLine[separatedDataLine.length - 1]);
+            numberOfAttribute = separatedDataLine.length;
+
+            while((currentLine = buffor.readLine()) != null) {
+                separatedDataLine = currentLine.split(separator);
+                classTypes.add(separatedDataLine[separatedDataLine.length - 1]);
+            }
+        } catch (IOException e) {
+            Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null,e);
+        }
+    }
+
+    private void getDataOfFile() {
+        data = new HashMap<>();
+        for(String obj : classTypes)
+        {
+                data.put(getDataOfColumns(obj), obj.toString());
+        }
+        //System.out.println("Dane wczytane poprawnie!");
+    }
+
+
+    private ArrayList<ArrayList<Double>> getDataOfColumns(String classType) {
+        BufferedReader buffor;
+        String currentLine;
+        String[] separatedDataLine;
+        ArrayList<ArrayList<Double>> columnsData = new ArrayList<ArrayList<Double>>();
+        for (int i = 0; i < numberOfAttribute - 1; i++) {
+            columnsData.add(new ArrayList<Double>());
+        }
+
+        try {
+            buffor = new BufferedReader(new FileReader(file));
+
+            while((currentLine = buffor.readLine()) != null) {
+                separatedDataLine = currentLine.split(separator);
+                if ((separatedDataLine[separatedDataLine.length - 1]).equals(classType)) {
+                    for (int i = 0; i < separatedDataLine.length-1; i++) {
+                        columnsData.get(i).add(Double.parseDouble(separatedDataLine[i]));
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null,e);
+        }
+
+        for (int i = 0; i < columnsData.size(); i++) {
+            Collections.sort(columnsData.get(i));
+        }
+        return columnsData;
+    }
 }
